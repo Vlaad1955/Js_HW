e2ac259a5f9a03c3b3b95f8419fe5ef013b761c0
Vlaad1955 <wylf1312@gmail.com>
2023-11-21 15:30:36 +0200

    add Js_1.js

---
+++ JsHw_1/Js_1.js
@@ -0,0 +1,33 @@
+console.log(s1 = "hello")
+console.log(s2 = 'owu')
+console.log(s3 = 'com')
+console.log(s1 = 'ua') //використав першу зміну, просто задав інше значення. Я це зробив осмислено, не помилково.
+console.log(n1 = 1)
+console.log(n2 = 10)
+console.log(n3 = -999)
+console.log(n4 = 123)
+console.log(n5 = 3.14)
+console.log(n6 = 2.7)
+console.log(n7 = 16)
+
+
+firstName = "Владислав"
+middleName = "Володимирович"
+lastName = "Рибалко"
+console.log(`${lastName} ${firstName} ${middleName}`)
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+var firstName = prompt('Введіть ім\'я:');
+var middleName = prompt('Введіть по-батькові:');
+var age = prompt('Введіть ваш вік:');
+console.log('Ім\'я:', firstName);
+console.log('По-батькові:', middleName);
+console.log('Вік:', age);
+
